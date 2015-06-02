b15f7bd2b4fd7849f8b0555b5e81a34e5a3d6901
Thomas Ruoff <thomasr@brandwatch.com>
2015-06-02 09:38:01 +0200

    new dist

--- dist/selleckt.js
+++ dist/selleckt.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.selleckt=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.selleckt = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 var KEY_CODES = {
@@ -162,9 +162,8 @@ MultiSelleckt.prototype.selectItem = function(item, options){
 
     if (!options.silent) {
         this.updateOriginalSelect();
+        this.trigger('itemSelected', item);
     }
-
-    this.trigger('itemSelected', item);
 };
 
 MultiSelleckt.prototype.updateOriginalSelect = function(){
@@ -1075,9 +1074,8 @@ _.extend(SingleSelleckt.prototype, {
 
         if (!options.silent) {
             this.$originalSelectEl.val(item.value).trigger('change', {origin: 'selleckt'});
+            this.trigger('itemSelected', item);
         }
-
-        this.trigger('itemSelected', item);
     },
 
     selectItemByValue: function(value, options) {
