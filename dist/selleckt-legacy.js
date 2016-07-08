372fa2baa225305d19ec7cea3d292e8cd108c3d8
Luiz Filho <lfilho@gmail.com>
2016-07-08 12:22:59 +0200

    Add .on and .off aliases to MicroEvent

--- dist/selleckt-legacy.js
+++ dist/selleckt-legacy.js
@@ -25,22 +25,28 @@ module.exports = KEY_CODES;
 */
 'use strict';
 
-var MicroEvent  = function(){};
+var MicroEvent = function(){};
 MicroEvent.prototype = {
-    bind    : function(event, fct){
+    bind: function(event, fct){
         this._events = this._events || {};
-        this._events[event] = this._events[event]   || [];
+        this._events[event] = this._events[event] || [];
         this._events[event].push(fct);
     },
-    unbind  : function(event, fct){
+    on: this.bind,
+    unbind: function(event, fct){
         this._events = this._events || {};
-        if( event in this._events === false  ) { return; }
+        if (event in this._events === false) {
+            return;
+        }
         this._events[event].splice(this._events[event].indexOf(fct), 1);
     },
-    trigger : function(event /* , args... */){
+    off: this.unbind,
+    trigger: function(event /* , args... */){
         this._events = this._events || {};
-        if( event in this._events === false  ) { return; }
-        for(var i = 0; i < this._events[event].length; i++){
+        if (event in this._events === false) {
+            return;
+        }
+        for (var i = 0; i < this._events[event].length; i++) {
             this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
         }
     }
@@ -54,9 +60,9 @@ MicroEvent.prototype = {
  * @param {Object} the object which will support MicroEvent
 */
 MicroEvent.mixin = function(destObject){
-    var props  = ['bind', 'unbind', 'trigger'];
-    for(var i = 0; i < props.length; i ++){
-        destObject.prototype[props[i]]  = MicroEvent.prototype[props[i]];
+    var props = ['bind', 'on', 'unbind', 'off', 'trigger'];
+    for (var i = 0; i < props.length; i++) {
+        destObject.prototype[props[i]] = MicroEvent.prototype[props[i]];
     }
 };
 
